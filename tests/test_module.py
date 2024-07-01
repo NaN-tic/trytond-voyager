ebb32a7a534778241b065173702b97e4d007a865
Juanjo Garcia <juanjo.garcia@nan-tic.com>
2024-07-01 12:11:29 +0200

    FIX tests. Add missing activate_module

--- tests/test_module.py
+++ tests/test_module.py
@@ -1,11 +1,17 @@
 # This file is part voyager module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
-from trytond.tests.test_tryton import ModuleTestCase
+from trytond.tests.test_tryton import ModuleTestCase, activate_module
 
 
 class VoyagerTestCase(ModuleTestCase):
     'Test Voyager module'
     module = 'voyager'
+
+    @classmethod
+    def setUpClass(cls):
+        super(VoyagerTestCase, cls).setUpClass()
+        activate_module('sale')
+        activate_module('web_shop')
 
 del ModuleTestCase
