ab8a889071336ed721cc2e7563512051919c8337
Ismael Marcos Martínez <ismael.marcos@nan-tic.com>
2026-05-12 16:44:00 +0200

    Fix commit ed63b14 & 0caa9f0
    - On method 'url': Remove language-based domains when searching URIs and replace it with canonical_uri.
    - On field 'canonical_uri': if nothing found, get itself.

--- voyager.py
+++ voyager.py
@@ -822,14 +822,12 @@ class Endpoint(Component):
         pool = Pool()
         VoyagerURI = pool.get('www.uri')
 
-        transaction = Transaction()
-
         values = {}
         uri_values = {}
         voyager_uri = None
 
         site = None
-        context = transaction.context.get('voyager_context')
+        context = Transaction().context.get('voyager_context')
         if hasattr(context, 'site'):
             site = context.site
 
@@ -866,10 +864,6 @@ class Endpoint(Component):
                             ('site', '=', site.id),
                             ('endpoint.model', '=', cls.__name__),
                             ('resource', '=', str(resource)),
-                            ['OR',
-                                ('language.code', '=', transaction.language),
-                                ('language', '=', None),
-                            ],
                         ], limit=1)
 
                         if voyager_uris:
@@ -883,10 +877,6 @@ class Endpoint(Component):
             voyager_uris = VoyagerURI.search([
                 ('site', '=', site.id),
                 ('endpoint.model', '=', cls.__name__),
-                ['OR',
-                    ('language.code', '=', transaction.language),
-                    ('language', '=', None),
-                ],
                 ], limit=1)
             if voyager_uris:
                 voyager_uri = voyager_uris[0]
@@ -895,6 +885,7 @@ class Endpoint(Component):
             values.update(site.to_url_prefix(cls, values))
 
         if voyager_uri:
+            voyager_uri = voyager_uri.canonical_uri
             parsed = urlparse(voyager_uri.uri)
             query = dict(parse_qsl(parsed.query))
             query.update(uri_values)
@@ -962,7 +953,7 @@ class VoyagerURI(DeactivableMixin, ModelSQL, ModelView):
                 ('id', '=', self.id),
             ],
             ('language.code', '=', language)])
-        return related and related[0] or None
+        return related and related[0]
 
     @classmethod
     def get_canonical_uri(cls, uris, name):
@@ -973,8 +964,7 @@ class VoyagerURI(DeactivableMixin, ModelSQL, ModelView):
                 result[uri.id] = uri.main_uri._get_canonical_uri()
             elif uri.related_uris:
                 result[uri.id] = uri._get_canonical_uri()
-            else:
-                result[uri.id] = None
+            result.setdefault(uri.id, uri)
         return result
 
     @classmethod
