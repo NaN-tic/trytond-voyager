e63334fe1cb939b67f564a6f65a3f215bb08b749
Ismael Marcos Martínez <ismael.marcos@nan-tic.com>
2026-06-04 13:15:56 +0200

    Fix 'str has no attribute get_href'.

--- voyager.py
+++ voyager.py
@@ -1186,7 +1186,7 @@ class VoyagerURI(DeactivableMixin, ModelSQL, ModelView):
     def get_href(self):
         pool = Pool()
 
-        canonical_uri = self.uri.canonical_uri
+        canonical_uri = self.canonical_uri
         resource = canonical_uri.resource
 
         try:
